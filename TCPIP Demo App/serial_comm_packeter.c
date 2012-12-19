9561ca78d04dce3e3656ff92447001d3143e6582
lzf <4366955@qq.com>
2012-12-19 19:18:24 +0800

    添加了新的字符同步帧通信代码

--- TCPIP Demo App/serial_comm_packeter.c
+++ TCPIP Demo App/serial_comm_packeter.c
@@ -13,9 +13,6 @@
 #define STREAM_NORMAL 1
 #define STREAM_IN_ESC 2
 
-DATA_RX_PACKET_T rx_pack;
-DATA_TX_PACKET_T tx_pack;
-
 
 #define  STREAM_START        0x0F
 #define  STREAM_END          0xF0
@@ -25,9 +22,18 @@ DATA_TX_PACKET_T tx_pack;
 //除了以上特殊字符外，其他都是原始字符
 
 
+
+
+RX_PACKS_CTL_T  rx_ctl;
+DATA_TX_CTL_T   tx_ctl;
+
+
 typedef unsigned short   WORD;
 
-#define BYTES_TO_WORD(bytes)            ((((WORD)((bytes)[1]))<<8)|(bytes)[0])
+#define GET_OFFSET_MEM_OF_STRUCT(type,member)   (&(((type *)0)->member) - ((type *)0))
+
+#define LSB_BYTES_TO_WORD(bytes)            ((((WORD)((bytes)[1]))<<8)|(bytes)[0])
+#define HSB_BYTES_TO_WORD(bytes)            ((((WORD)((bytes)[0]))<<8)|(bytes)[1])
 
 unsigned int CRC16(unsigned char *Array,unsigned int Len)
 {
@@ -75,101 +81,305 @@ unsigned int CRC162(unsigned char *Array,unsigned int Len)
 	return CRC;
 }
 
- void pack_prase_in(unsigned char ch)
- {
-   if(rx_pack.finished) {
-     return ;
+
+void rx_find_next_empty_buffer(void)
+{
+	unsigned int i;
+	rx_ctl.pcurrent_rx = NULL;
+	for(i=0;i<RX_PACKS_MAX_NUM;i++) {
+		if(!(rx_ctl.rx_packs[i].finished)) {
+			rx_ctl.pcurrent_rx = &rx_ctl.rx_packs[i];
+			break;
+		}
+	}
+}
+
+
+void pack_prase_in(unsigned char ch)
+{
+   DATA_RX_PACKET_T * prx;
+   if(rx_ctl.pcurrent_rx == NULL) {
+	   rx_find_next_empty_buffer();
+	   if(rx_ctl.pcurrent_rx == NULL) {
+		   return ;
+	   }
    }
-   switch(rx_pack.state)
+   prx = rx_ctl.pcurrent_rx;
+   if(prx->finished) {
+	   rx_find_next_empty_buffer();
+	   prx = rx_ctl.pcurrent_rx;
+	   if(prx == NULL) {
+		   return ;
+	   }
+   }
+   switch(prx->state)
    {
    case STREAM_IDLE:
      if(ch == STREAM_START) {
-       rx_pack.state = STREAM_NORMAL;
-       rx_pack.index = 0;
+       prx->state = STREAM_NORMAL;
+       prx->index = 0;
      }
      break;
    case STREAM_NORMAL:
      if(ch == STREAM_ESCAPE) {
-       rx_pack.state = STREAM_IN_ESC;
+       prx->state = STREAM_IN_ESC;
      } else if(ch == STREAM_START) {
-       rx_pack.state = STREAM_IDLE;
+       prx->state = STREAM_IDLE;
      } else if(ch == STREAM_END) {
-       if(rx_pack.index >= 3) {
-         unsigned int crc = CRC16(rx_pack.buffer,rx_pack.index-2);
-         if(crc == BYTES_TO_WORD(&rx_pack.buffer[rx_pack.index-2])) {
-			 rx_pack.index -= 2;
-             rx_pack.finished = 1;
+       if(prx->index >= 3) {
+         unsigned int crc = CRC16(prx->buffer,prx->index-2);
+         if(crc == LSB_BYTES_TO_WORD(&prx->buffer[prx->index-2])) {
+			 prx->index -= 2;
+			 prx->look_up_times = 0;
+             prx->finished = 1;
          }
        }
-       rx_pack.state = STREAM_IDLE;
+	   prx->state = STREAM_IDLE;
      } else {
-       rx_pack.buffer[rx_pack.index++] = ch;
+       prx->buffer[prx->index++] = ch;
      }
      break;
    case STREAM_IN_ESC:
      if(ch == STREAM_ESCAPE) {
-       rx_pack.buffer[rx_pack.index++] = STREAM_ESCAPE;
-       rx_pack.state = STREAM_NORMAL;
+       prx->buffer[prx->index++] = STREAM_ESCAPE;
+       prx->state = STREAM_NORMAL;
      } else if(ch == STREAM_ES_S) {
-       rx_pack.buffer[rx_pack.index++] = STREAM_START;
-       rx_pack.state = STREAM_NORMAL;
+       prx->buffer[prx->index++] = STREAM_START;
+       prx->state = STREAM_NORMAL;
      } else if(ch == STREAM_ES_E) {
-       rx_pack.buffer[rx_pack.index++] = STREAM_END;
-       rx_pack.state = STREAM_NORMAL;
+       prx->buffer[prx->index++] = STREAM_END;
+       prx->state = STREAM_NORMAL;
      } else {
-       rx_pack.state = STREAM_IDLE;
+       prx->state = STREAM_IDLE;
      }
      break;
    default:
-     rx_pack.state = STREAM_IDLE;
+     prx->state = STREAM_IDLE;
      break;
    }
    //溢出判断
-   if(rx_pack.index >= sizeof(rx_pack.buffer)) {
-     rx_pack.state = STREAM_IDLE;
+   if(prx->index >= sizeof(prx->buffer)) {
+     prx->state = STREAM_IDLE;
    }
- }
+}
 
 
- unsigned int prase_in_buffer(unsigned char * src,unsigned int len)
- {
+
+
+
+
+/*************************************************
+ * 找到空的发送缓冲，意思是找到可以填充数据的缓冲
+ */
+DATA_TX_PACKET_T * find_next_empty_tx_buffer(void)
+{
+	DATA_TX_PACKET_T * ptx = NULL;
+	unsigned int i;
+	for(i=0;i<TX_PACKS_MAX_NUM;i++) {
+		if(!tx_ctl.packet[i].finished) {
+			ptx = &tx_ctl.packet[i];
+		}
+	}
+	return ptx;
+}
+
+/*************************************************
+ * 找到已经填充完成的指针，然后用串口等等发送出去
+ */
+DATA_TX_PACKET_T * find_ready_tx_buffer(void)
+{
+	DATA_TX_PACKET_T * ptx = NULL;
+	unsigned int i;
+	for(i=0;i<TX_PACKS_MAX_NUM;i++) {
+		if(tx_ctl.packet[i].finished) {
+			ptx = &tx_ctl.packet[i];
+		}
+	}
+	return ptx;
+}
+
+
+
+DATA_TX_PACKET_T * prase_in_buffer(unsigned char * src,unsigned int len)
+{
+	 DATA_TX_PACKET_T * ptx = NULL;
 	 unsigned int i = 0;
 	 if(len == 0) {
-		 return 0;
-	 } else if(tx_pack.finished) {
-		 return 0;
-	 } else {
-		 unsigned int crc = CRC162(src,len);
-		 tx_pack.index = 0;
-		 tx_pack.buffer[tx_pack.index++] = STREAM_START;
+		 return NULL;
+	 }
+	 if((ptx = find_next_empty_tx_buffer()) == NULL) {
+		 return NULL;
+	 }
+	 if(1) {
+		 unsigned int crc = CRC16(src,len);
+		 ptx->index = 0;
+		 ptx->buffer[ptx->index++] = STREAM_START;
 		 while(len--) {
 			 unsigned char reg = src[i++];
-			 if(tx_pack.index >= (sizeof(tx_pack.buffer) - 4)) {
+			 if(ptx->index >= (sizeof(ptx->buffer) - 4)) {
 				 //溢出判断,2字节CRC，一个结束位,一个转义字符
-				 tx_pack.index = 0;
+				 ptx->index = 0;
 				 break;
 			 }
 			 if(reg == STREAM_START) {
-				 tx_pack.buffer[tx_pack.index++] = STREAM_ESCAPE;
-				 tx_pack.buffer[tx_pack.index++] = STREAM_ES_S;
+				 ptx->buffer[ptx->index++] = STREAM_ESCAPE;
+				 ptx->buffer[ptx->index++] = STREAM_ES_S;
 		     } else if(reg == STREAM_ESCAPE) {
-				 tx_pack.buffer[tx_pack.index++] = STREAM_ESCAPE;
-				 tx_pack.buffer[tx_pack.index++] = STREAM_ESCAPE;
+				 ptx->buffer[ptx->index++] = STREAM_ESCAPE;
+				 ptx->buffer[ptx->index++] = STREAM_ESCAPE;
 			 } else if(reg == STREAM_END) {
-				 tx_pack.buffer[tx_pack.index++] = STREAM_ESCAPE;
-				 tx_pack.buffer[tx_pack.index++] = STREAM_ES_E;
+				 ptx->buffer[ptx->index++] = STREAM_ESCAPE;
+				 ptx->buffer[ptx->index++] = STREAM_ES_E;
 			 } else {
-				 tx_pack.buffer[tx_pack.index++] = reg;
+				 ptx->buffer[ptx->index++] = reg;
 			 }
 		 }
-		 if(tx_pack.index > 0) {
-		     tx_pack.buffer[tx_pack.index++] = crc & 0xFF;
-		     tx_pack.buffer[tx_pack.index++] = crc >> 8;
-		     tx_pack.buffer[tx_pack.index++] = STREAM_END;
-		     tx_pack.finished = 1;
+		 if(ptx->index > 0) {
+		     ptx->buffer[ptx->index++] = crc & 0xFF;
+		     ptx->buffer[ptx->index++] = crc >> 8;
+		     ptx->buffer[ptx->index++] = STREAM_END;
+		     ptx->finished = 1;
 		 }
 	 }
-	 return tx_pack.index;
- }
+	 return ptx;
+}
 
+
+
+unsigned int tx_pack_and_send(unsigned char * src,unsigned int len)
+{
+	//立即发送，只有发完之后才能返回
+	DATA_TX_PACKET_T * ptx = prase_in_buffer(src,len);
+	if(ptx == NULL) {
+		return 0;
+	}
+	if(ptx->finished) {
+		//立即发送
+        unsigned int i;
+        for(i=0;i<ptx->index;i++) {
+            //send_uart1(ptx->buffer[i]);
+        }
+        ptx->finished = 0;
+	}
+	return ptx->index;
+}
+
+
+
+unsigned int tx_pack_and_post(unsigned char * src,unsigned int len)
+{
+	//不发送
+	DATA_TX_PACKET_T * ptx = prase_in_buffer(src,len);
+    //启动发送中断
+    if(ptx) {
+        return (ptx->finished)?len:0;
+    } else {
+        return 0;
+    }
+}
+
+
+
+
+DATA_RX_PACKET_T * GetFinishedPacket(void)
+{
+	unsigned int  i;
+	DATA_RX_PACKET_T * prx;
+	for(i=0;i<RX_PACKS_MAX_NUM;i++) {
+		prx = &rx_ctl.rx_packs[i]; 
+		if(prx->finished) {
+			break;
+		}
+	}
+	if(i == RX_PACKS_MAX_NUM) {
+		return NULL;
+	} else {
+		return prx;
+	}
+}
+
+
+void tx_free_useless_packet(unsigned int net_communication_count)
+{
+	unsigned int  i;
+	DATA_RX_PACKET_T * prx;
+	for(i=0;i<RX_PACKS_MAX_NUM;i++) {
+		prx = &rx_ctl.rx_packs[i]; 
+		if(prx->finished) {
+			if(prx->look_up_times == net_communication_count) {
+				prx->finished = 0; //所有人都看过了，结果没有人需要，则丢弃它。
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------
+// MODBUS读指令
+//-----------------------------------------------------------
+
+
+unsigned char modbus_read_multi_coils_request(unsigned int start_coils,unsigned int coils_num,unsigned char slave_device)
+{
+    struct modbus_read_coil_status_req_type
+    {
+		unsigned char slave_addr;
+		unsigned char function;
+		unsigned char start_addr_hi;
+		unsigned char start_addr_lo;
+		unsigned char reg_munber_hi;
+		unsigned char reg_number_lo;
+		unsigned char crc_hi;
+		unsigned char crc_lo;
+	};
+	struct modbus_read_coil_status_req_type tx;
+	unsigned int crc;
+	tx.slave_addr = slave_device;
+	tx.function = 0x01;  //READ_COIL_STATUS
+	tx.start_addr_hi = start_coils >> 8;
+	tx.start_addr_lo = start_coils & 0xFF;
+	tx.reg_munber_hi = coils_num >> 8;
+	tx.reg_number_lo = coils_num & 0xFF;
+	crc = CRC16((unsigned char *)&tx,sizeof(tx)-2);
+	tx.crc_hi = crc >> 8;
+	tx.crc_lo = crc & 0xFF;
+	return tx_pack_and_send((unsigned char *)&tx,sizeof(tx));
+}
+
+
+
+unsigned int modbus_prase_read_multi_coils_ack(unsigned char slave_device,unsigned char * rx_buffer,unsigned int len,unsigned int startbit,unsigned int count)
+{
+	typedef struct __modbus_force_multiple_coils_req_type
+	{
+		unsigned char slave_addr;
+		unsigned char function;
+		unsigned char start_addr_hi;
+		unsigned char start_addr_lo;
+		unsigned char quantiry_coils_hi;
+		unsigned char quantiry_coils_lo;
+		unsigned char byte_count;
+		unsigned char database;
+	} modbus_force_multiple_coils_req_type;
+	modbus_force_multiple_coils_req_type * pack = (modbus_force_multiple_coils_req_type *)rx_buffer;
+	if(len > sizeof(modbus_force_multiple_coils_req_type) && pack->slave_addr == slave_device) {
+		if(pack->function == 0x01) {  //READ_COIL_STATUS
+			if(startbit == HSB_BYTES_TO_WORD(&pack->start_addr_hi)) {
+				unsigned int count = HSB_BYTES_TO_WORD(&pack->start_addr_hi);
+				if(count > 0) {
+					//根据读到的数据，修改某些寄存器的位
+					return count;
+				}
+			}
+		}
+	}
+	return 0;
+}
 
--- TCPIP Demo App/serial_comm_packeter.h
+++ TCPIP Demo App/serial_comm_packeter.h
@@ -2,30 +2,58 @@
 #define __SERIAL_COMM_PACKER_H__
 
 
-#define  PACK_MAX_RX_SIZE   32
+
+#define  PACK_MAX_RX_SIZE   16
+#define  RX_PACKS_MAX_NUM   2
+#define  TX_PACKS_MAX_NUM   1
 
 
 typedef struct _DATA_RX_PACKET_T
 {
   unsigned char buffer[PACK_MAX_RX_SIZE];
+  unsigned int  look_up_times;  //别查看次数
   unsigned char index;
   unsigned char state : 4;
   unsigned char finished : 1;
 } DATA_RX_PACKET_T;
 
 
+
+typedef struct _RX_PACKS_CTL_T
+{
+	DATA_RX_PACKET_T   rx_packs[RX_PACKS_MAX_NUM];
+	DATA_RX_PACKET_T * pcurrent_rx;
+} RX_PACKS_CTL_T;
+
+
+
+
+
 typedef struct _DATA_TX_PACKET_T
 {
-  unsigned char buffer[PACK_MAX_RX_SIZE*2+2];
-  unsigned char index;
+  unsigned char buffer[PACK_MAX_RX_SIZE + PACK_MAX_RX_SIZE/2];
+  unsigned char index;  //发送的大小
   unsigned char finished : 1;
 } DATA_TX_PACKET_T;
 
+typedef struct _DATA_TX_CTL_T
+{
+	DATA_TX_PACKET_T   packet[TX_PACKS_MAX_NUM];
+	DATA_TX_PACKET_T * ptx_sending;
+} DATA_TX_CTL_T;
 
-//extern DATA_RX_PACKET_T rx_pack;
-//extern DATA_TX_PACKET_T tx_pack;
 
-//extern unsigned int prase_in_buffer(unsigned char * src,unsigned int len);
-//extern void pack_prase_in(unsigned char ch);
+extern RX_PACKS_CTL_T rx_ctl;
+extern DATA_TX_CTL_T  tx_ctl;
+
+
+extern void pack_prase_in(unsigned char ch);
+extern DATA_RX_PACKET_T * GetFinishedPacket(void);
+extern void tx_free_useless_packet(unsigned int net_communication_count);
+extern unsigned int tx_pack_and_send(unsigned char * src,unsigned int len);
+
+extern unsigned int modbus_prase_read_multi_coils_ack(unsigned char slave_device,unsigned char * rx_buffer,unsigned int len,unsigned int startbit,unsigned int count);
+extern unsigned char modbus_read_multi_coils_request(unsigned int start_coils,unsigned int coils_num,unsigned char slave_device);
+
 
 #endif
